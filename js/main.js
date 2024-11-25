47d31bacd5669e2122ad30f47585fa79f2d80b39
Kosmo <98766961+sohgottes@users.noreply.github.com>
2024-11-25 16:55:48 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const message = document.getElementById("contact-message");
+    message.style.animation = "blink 1.5s infinite alternate";
+});
